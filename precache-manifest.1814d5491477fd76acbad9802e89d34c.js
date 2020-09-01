37983171657f2f1e29d8c149316f80727b24ae7f
Barklim <kliment.barkalov@gmail.com>
2020-09-02 00:07:23 +0300

    Updates

---
+++ precache-manifest.1814d5491477fd76acbad9802e89d34c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed46c2a375c113acd00404f6c70bc474",
+    "url": "/docskeeperweb/index.html"
+  },
+  {
+    "revision": "e3216db0cd53a96c6460",
+    "url": "/docskeeperweb/static/css/2.d54bb455.chunk.css"
+  },
+  {
+    "revision": "e3216db0cd53a96c6460",
+    "url": "/docskeeperweb/static/js/2.18aeedf4.chunk.js"
+  },
+  {
+    "revision": "c346956c1449420dcbe7f2a51cdfc9c4",
+    "url": "/docskeeperweb/static/js/2.18aeedf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "880f664a771431b87bce",
+    "url": "/docskeeperweb/static/js/3.5bcfac70.chunk.js"
+  },
+  {
+    "revision": "d2db99132785b3d2d51e170b10628c0b",
+    "url": "/docskeeperweb/static/js/3.5bcfac70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "028a2bda22928c5a88fd",
+    "url": "/docskeeperweb/static/js/4.d8186c22.chunk.js"
+  },
+  {
+    "revision": "9e16d1e76e42985a69c3",
+    "url": "/docskeeperweb/static/js/5.0bab0f25.chunk.js"
+  },
+  {
+    "revision": "af6c4412e947c3d13ff6",
+    "url": "/docskeeperweb/static/js/main.8cf79202.chunk.js"
+  },
+  {
+    "revision": "45e6a70745bee18d2f51",
+    "url": "/docskeeperweb/static/js/runtime-main.2bfcf8c6.js"
+  }
+]);
